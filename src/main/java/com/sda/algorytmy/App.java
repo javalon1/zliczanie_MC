a4617fa19508a91f072cfced3d89fd9c16fcc90d
ciechann <emgasuk@gmail.com>
2019-05-12 14:51:00 +0100

    wystepowanie

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,12 +2,22 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        int[] a = {4, 1, 6, 1, 1, 4, 3, 3, 1};
+
+        System.out.println("Liczba wystapien wartosci x: " + wystapienie(a));
+
+    }public static int wystapienie (int[] a){
+        int x = 1;
+        int wynik = 0;
+
+        for(int i=0; i<a.length; i++){
+            if(x==a[i]){
+                wynik = wynik + 1;
+            }
+        }return wynik;
     }
 }
